895b684f4afe9bd9d829a6e2f38643c82c6811f0
Irosh Mathews <praneeth@axis-tech.co>
2024-10-04 15:48:31 +0530

    Added FrontEnd URL

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors()); // Allow cross-origin requests
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000', // TODO: update origin for deployment
+    origin: 'https://axisbluffers.netlify.app/', // TODO: update origin for deployment
     methods: ['GET', 'POST']
   }
 });
